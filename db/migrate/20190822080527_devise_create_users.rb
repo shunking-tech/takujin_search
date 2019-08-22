50457745bb941931f3df14a2f249fcaddf3eb0a1
shunking-tech <shnktech@gmail.com>
2019-08-22 17:16:34 +0900

    Update 20190822080527_devise_create_users.rb
    カラムの追加

--- db/migrate/20190822080527_devise_create_users.rb
+++ db/migrate/20190822080527_devise_create_users.rb
@@ -4,7 +4,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: false
+      t.string :name,               null: false
+      t.integer :type,              null: false
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable

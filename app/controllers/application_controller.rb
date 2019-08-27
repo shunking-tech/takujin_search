e0274a4145c7ca8a0748e19ad0f843e789f04515
shunking-tech <shnktech@gmail.com>
2019-08-27 17:25:18 +0900

    sign up function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # sign_upのときに、nameとpurposeをストロングパラメータに追加する
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :purpose])
+    # account_updateのときに、nameとpurposeをストロングパラメータに追加する
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :purpose])
+  end
 end

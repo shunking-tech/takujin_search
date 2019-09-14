33cee0ebca24159d46737f514bdb47038d3c05cb
shunking-tech <shnktech@gmail.com>
2019-09-14 11:48:46 +0900

    teamを登録する処理

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -8,4 +8,14 @@ class TeamsController < ApplicationController
     @team = Team.new
   end
 
+  def create
+    team = Team.create(team_params)
+    redirect_to team_path(team)
+  end
+
+  private
+
+    def team_params
+      params.require(:team).permit(:team_name, :motivation, :activity_times, :area, :want_gender, :want_experience_period, :want_performance, :searching).merge(user_id: current_user.id)
+    end
 end
